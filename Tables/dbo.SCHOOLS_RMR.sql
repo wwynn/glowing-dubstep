12333ae410e99befe7fd1b8dd131c435a51bb67c
Bill <wwynn@ccedresults.org>
2013-07-30 15:39:00 -0700

    Changed SchoolCode name and datatype
    Changed [School Code] to SchoolCode and datatype to string.

--- Tables/dbo.SCHOOLS_RMR.sql
+++ Tables/dbo.SCHOOLS_RMR.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[SCHOOLS_RMR]
 (
 [District Code] [float] NULL,
 [District] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[School Code] [float] NULL,
+[SchoolCode] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [In 2011-12 set?] [float] NULL,
 [School Name 1] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [School Name 2] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
